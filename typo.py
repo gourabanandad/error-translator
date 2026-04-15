3322ed6c5db0c89cc9c6bdf4c58f79efd15567c9
gourabanandad <gourabanandadatta2003@gmail.com>
2026-04-15 20:01:31 +0530

    Delete the typo.py

--- typo.py
+++
@@ -1,9 +0,0 @@
-import error_translator.auto
-
-# We define a complex variable
-maximum_user_connections = 100
-
-print("Booting up server...")
-
-# Oh no, we made a typo!
-print(maximum_user_connectons)
